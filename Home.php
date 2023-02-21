dd0518426e42e7a2aa0cda7f808ac0fefa46f5bb
Giacomo Previtali <giacomoprevitali26@gmail.com>
2023-02-21 10:19:17 +0100

    Continuo Homepage

--- Home.php
+++ Home.php
@@ -4,24 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script type='text/javascript' src='script.js'></script>
+    <link rel="stylesheet" href="style.css" />
     <title>Home</title>
 </head>
 <body>
+<nav class="navbar navbar-dark bg-primary">
+ <h1 class="title"> D.V.R.</h1>
+</nav>
     
-    <input id="pesoEff"     placeholder="Peso Effettivo (Kg)"><br>
-    <input id="AltIn"       placeholder="Altezza iniziale (Cm)"><br>
-    <input Id="DistVert"    placeholder="Distanza verticale (Cm)"><br>
-    <input Id="DistOrizz"   placeholder="Distanza orizzontale (Cm)"><br>
-    <input Id="DistAngo"    placeholder="Distanza angolare"><br>
-    <input Id="PresaC"      placeholder="Presa sul carico"><br>
-    <input Id="Freq"        placeholder="Frequenza (volte al minuto)"><br>
-    <input Id="Dur"         placeholder="Durata (Ore)">  
-    <button onclick="calcola()">Calcola</button>
-
     <?php
     session_start();
     echo "Ciao, ". $_SESSION['Nome']." ".$_SESSION['Cognome'];
-    ?>
+    ?><br>
+    <a href="new.php">
+    <button type="button" class="btn btn-outline-primary">Compila un nuovo DVR</button>
+    </a>
+    <br>
+    <a href="view.php">
+    <button type="button" class="btn btn-outline-warning">Visualizza</button>
+    </a>
+    
+   
 </body>
 </html>
---
+++ new.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input id="pesoEff"     placeholder="Peso Effettivo (Kg)"><br>
+    <input id="AltIn"       placeholder="Altezza iniziale (Cm)"><br>
+    <input Id="DistVert"    placeholder="Distanza verticale (Cm)"><br>
+    <input Id="DistOrizz"   placeholder="Distanza orizzontale (Cm)"><br>
+    <input Id="DistAngo"    placeholder="Distanza angolare"><br>
+    <input Id="PresaC"      placeholder="Presa sul carico"><br>
+    <input Id="Freq"        placeholder="Frequenza (volte al minuto)"><br>
+    <input Id="Dur"         placeholder="Durata (Ore)">  
+    <button onclick="calcola()">Calcola</button>
+</body>
+</html>
